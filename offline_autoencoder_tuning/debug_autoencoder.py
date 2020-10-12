ce9e110401840899c11bc16355091c337e2e9ef1
luigiberducci <luigi.berd@gmail.com>
2020-10-12 10:22:26 +0200

    using h5 file

--- offline_autoencoder_tuning/debug_autoencoder.py
+++ offline_autoencoder_tuning/debug_autoencoder.py
@@ -1,11 +1,20 @@
 import models
 import numpy as np
+import h5py as h5
+import os
 
 encoded_obs_dim = 128
 lidar_shape = 1000
 
 encode = models.LidarEncoder(output_dim=encoded_obs_dim)
 decode = models.LidarDecoder(output_dim=lidar_shape)
+
+output_dir = "out"
+dataset_filename = "dataset_random_starts_austria_2000episodes_1000maxobs.h5"
+data = h5.File(os.path.join(output_dir, dataset_filename), "r")
+print(data.keys())
+
+
 
 obs = {}
 obs['lidar'] = np.zeros((1, 1000))
