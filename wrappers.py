8a19340bee884155667458f0ce558afef3387ea7
luigiberducci <luigi.berd@gmail.com>
2020-12-10 18:01:26 +0100

    add close method to dreamer env wrapper

--- wrappers.py
+++ wrappers.py
@@ -69,6 +69,9 @@ class SingleForkedRaceCarWrapper:
   def render(self, **kwargs):
     return self._env.render(**kwargs)
 
+  def close(self):
+    self._env.close()
+
 
 class SingleRaceCarWrapper:
 
