062d602ecfe1390b76d8267c1f736bedf8337d2e
luigiberducci <luigi.berd@gmail.com>
2020-12-15 09:42:19 +0100

    correct output decoder distr

--- models.py
+++ models.py
@@ -151,10 +151,11 @@ class MLPLidarDecoder(tools.Module):
     x = self.get('dense1', tfkl.Dense, units=2*self._depth, activation="linear")(x)
     x = self.get('dense2', tfkl.Dense, units=2*self._depth, activation=self._act)(x)
     x = self.get('dense3', tfkl.Dense, units=4*self._depth, activation=self._act)(x)
-    x = self.get('dense4', tfkl.Dense, units=self._shape[0], activation=self._act)(x)
-    mean = tf.reshape(x, tf.concat([tf.shape(features)[:-1], self._shape], 0))
-    return tfd.Independent(tfd.Normal(mean, 1), len(self._shape))
-
+    params = tfpl.IndependentNormal.params_size(self._shape[0])
+    x = self.get('params', tfkl.Dense, units=params, activation=tf.nn.leaky_relu)(x)
+    x = self.get('dist', tfpl.IndependentNormal, event_shape=self._shape[0])(x)
+    dist = tfd.BatchReshape(x, batch_shape=features.shape[:2])
+    return dist
 
 class LidarDecoder(tools.Module):
 
