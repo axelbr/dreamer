91f0d0c38ae7bd5da9525a498028e9be005bbd96
luigiberducci <luigi.berd@gmail.com>
2021-02-18 14:18:55 +0100

    add new actor distribution without tanh scaling but linear mean, then tanh bijector

--- models.py
+++ models.py
@@ -267,12 +267,20 @@ class ActionDecoder(tools.Module):
     x = features
     for index in range(self._layers):
       x = self.get(f'h{index}', tfkl.Dense, self._units, self._act)(x)
-    if self._dist == 'tanh_normal':   # Original from Dreamer
+    if self._dist == 'tanh_normal':  # Original from Dreamer
       # https://www.desmos.com/calculator/rcmcf5jwe7
       x = self.get(f'hout', tfkl.Dense, 2 * self._size)(x)
       mean, std = tf.split(x, 2, -1)
       mean = self._mean_scale * tf.tanh(mean / self._mean_scale)
       std = tf.nn.softplus(std + raw_init_std) + self._min_std
+      dist = tfd.Normal(mean, std)
+      dist = tfd.TransformedDistribution(dist, tools.TanhBijector())
+      dist = tfd.Independent(dist, 1)
+      dist = tools.SampleDist(dist)
+    elif self._dist == 'unscaled_tanh_normal':   # Like in dreamer, but with linear mean
+      x = self.get(f'hout', tfkl.Dense, 2 * self._size)(x)
+      mean, std = tf.split(x, 2, -1)
+      std = tf.nn.softplus(std) + self._min_std
       dist = tfd.Normal(mean, std)
       dist = tfd.TransformedDistribution(dist, tools.TanhBijector())
       dist = tfd.Independent(dist, 1)
